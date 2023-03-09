23a28a68a69be3c47d4dffea52068b7eddd78e8a
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-09 08:03:43 +0630

    add own gradient

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,8 +11,8 @@ const Hero = () => {
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915eff]"></div>
-          <div className="w-1 sm:h-80 h-40 violet-gradient"></div>
+          <div className="w-5 h-5 rounded-full bg-[#00ff00]"></div>
+          <div className="w-1 sm:h-80 h-40 own-gradient"></div>
         </div>
 
         <div>
