f340f3835af40c084a405b0050c2da963f2b2838
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-10 23:52:29 +0630

    before particles fix

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,8 +4,8 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
-const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+const Computers = () => {
+  const computer = useGLTF("./cyberpunk_laptop/scene.gltf");
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
@@ -20,47 +20,49 @@ const Computers = ({ isMobile }) => {
       />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.75, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        scale={2.2}
+        position={[1, -1, 0]}
+        rotation={[1.06, -1, 1]}
       />
     </mesh>
   );
 };
 
 const ComputersCanvas = () => {
-  const [isMobile, setIsMobile] = useState(false);
+  // const [isMobile, setIsMobile] = useState(false);
 
-  useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
+  // useEffect(() => {
+  //   const mediaQuery = window.matchMedia("(max-width: 500px)");
 
-    setIsMobile(mediaQuery.matches);
+  //   setIsMobile(mediaQuery.matches);
 
-    const handleMediaQueryChange = (e) => {
-      setIsMobile(e.matches);
-    };
+  //   const handleMediaQueryChange = (e) => {
+  //     setIsMobile(e.matches);
+  //   };
 
-    mediaQuery.addEventListener("change", handleMediaQueryChange);
+  //   mediaQuery.addEventListener("change", handleMediaQueryChange);
 
-    return () => {
-      mediaQuery.removeEventListener("change", handleMediaQueryChange);
-    };
-  }, []);
+  //   return () => {
+  //     mediaQuery.removeEventListener("change", handleMediaQueryChange);
+  //   };
+  // }, []);
 
   return (
     <Canvas
-      frameloop="demand"
+      frameloop="always"
       shadows
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
+          autoRotate
+          autoRotateSpeed={5}
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers isMobile={isMobile} />
+        <Computers />
       </Suspense>
       <Preload all />
     </Canvas>
